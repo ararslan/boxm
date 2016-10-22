eb96f550c81ea09cd6668c0a6982f4c3793f9de0
Alex Arslan <ararslan@comcast.net>
2016-10-22 12:56:45 -0700

    Add F approximation, Chi^2 fix, and refs

--- R/boxmtest.R
+++ R/boxmtest.R
@@ -3,13 +3,13 @@ boxmtest <- function(X, groups) {
         stop("X must be a numeric matrix or data frame")
     }
     if (!is.factor(groups)) {
-        stop("groups must be a factor.")
+        stop("groups must be a factor")
     }
     if (length(groups) != nrow(X)) {
         stop("groups must have length equal to the number of rows in X")
     }
     if (nlevels(groups) < 2) {
-        stop("groups has fewer than 2 levels.")
+        stop("groups has fewer than 2 levels")
     }
 
     N <- NROW(X)
@@ -23,11 +23,28 @@ boxmtest <- function(X, groups) {
     Si <- lapply(levels(groups), function(i) cov(X[groups == i, ]))
     Sp <- Reduce("+", lapply(1:g, function(i) (nl[[i]] - 1) * Si[[i]])) / N
 
-    u <- sum(1 / (n - 1) - 1 / N) * ((2 * p^2 + 3 * p - 1) / (6 * (p + 1) * (g - 1)))
+    u <- (sum(1 / (n - 1)) - 1 / N) * ((2 * p^2 + 3 * p - 1) / (6 * (p + 1) * (g - 1)))
     M <- N * log(det(Sp)) - sum((n - 1) * sapply(Si, function(x) log(det(x))))
 
-    stat <- (1 - u) * M
-    df <- p * (p + 1) * (g - 1) / 2
+    df <- as.integer(p * (p + 1) * (g - 1) / 2)
 
-    list(statistic = stat, df = df, pvalue = pchisq(stat, df))
+    if (all(n > 20) && p <= 5 && g <= 5) {
+        stat <- (1 - u) * M
+        return(list(statistic = stat, df = df, pvalue = pchisq(stat, df, lower.tail = FALSE)))
+    } else {
+        a <- (sum(1 / (n - 1)^2) - 1 / N^2) * (p^2 + p - 2) / (6 * (g - 1))
+        df2 <- as.integer((df + 2) / abs(a - u^2))
+
+        if (a > u^2) {  # Pearson's type VI curve
+            b <- df / (1 - u - df / df2)
+            stat <- M / b
+        } else {        # Pearson's type I curve
+            b <- df / (1 - u - 2 / df2)
+            stat <- df2 * M / (df * (b - M))
+        }
+        # Hopefully the type III case (a == u^2) will be taken care of by the chisq case
+        # above, since df2 will be infinite so the statistic is ~ chisq(df)
+
+        return(list(stat = stat, df = c(df, df2), pvalue = pf(stat, df, df2, lower.tail = FALSE)))
+    }
 }
--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # boxm
 
 An [R](https://www.r-project.org) package for performing Box's test for equality of covariance
-matrices, as described in *Applied Multivariate Statistical Analysis* by Johnson and Wichern.
+matrices.
 
 I'll probably add documentation and tests at some point.
+
+### References:
+
+1. Box, G. E. P., "A General Distribution Theory for a Class of Likelihood Criteria."
+  *Biometrika*, **36** (1949), 317-346.
+
+2. Johnson, R. A., Wichern, D. W., *Applied Multivariate Statistical Analysis*. 6th ed.
+  Upper Saddle River, NJ: Prentice Hall, 2007.
